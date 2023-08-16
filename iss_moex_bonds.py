fc3031bb7ad649fa2cbbe047ee6a730980a0de84
Vladislav Balabaev <vv.balabaev@gmail.com>
2023-08-16 22:23:47 +0700

    delete utils.py

--- iss_moex_bonds.py
+++ iss_moex_bonds.py
@@ -5,6 +5,7 @@ import pandas as pd
 import datetime as dt
 from tqdm import tqdm
 from urllib import parse
+from termcolor import colored
 
 from utils import *
 
@@ -14,6 +15,25 @@ columns_for_info = {
     'facevalue', 'listlevel', 'includedbymoex', 'issuesize', 'isqualifiedinvestors',
     'couponfrequency', 'couponpercent', 'couponvalue', 'typename'
 }
+
+
+def dt_now_str():
+    """
+    Get current datetime painted green in string format.
+    """
+    now = dt.datetime.now().isoformat(sep=" ", timespec="seconds")
+    return f"{colored(text=f'({now})', color='green')}"
+
+
+def pandify(json_object, json_key='securities', columns: list = None):
+    """
+    Transform json object to pd.Dataframe.
+    """
+    df = pd.DataFrame(json_object[json_key]['data'],
+                      columns=json_object[json_key]['columns'])
+    if columns:
+        df = df[columns]
+    return df
 
 
 def query(method: str, details: dict = None):
